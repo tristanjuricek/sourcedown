6738cbd78d1f1f12f05d2f0346d0f644019e8013
Tristan Juricek <mr.tristan@gmail.com>
2011-10-22 11:45:50 -0700

    Created the testing framework. Use "gradle rFT" to run.

--- src/test/scala/com/tristanhunt/sourcedown/RunSourcedownTests.scala
+++ src/test/scala/com/tristanhunt/sourcedown/RunSourcedownTests.scala
@@ -1,36 +1,105 @@
 /*
-    # The Sourcedown Test Runner
+  # The Sourcedown Test Runner
 
-    Excecutes sourcedown in various "input" test directories and outputs things
-    to a corresponding build folder. Once complete, we run a tree diff tool and
-    dump any changes we see.
+  Excecutes sourcedown in various "input" test directories and outputs things
+  to a corresponding build folder. Once complete, we run a tree diff tool and
+  dump any changes we see.
 
-    This will follow typical project build conventions and thus will not need
-    much configuration.
+  This will follow typical project build conventions and thus will not need
+  much configuration.
 */
 
-import java.io.File
+package com.tristanhunt.sourcedown
+
+import scalax.file._
+import sys.process._
 
 object RunSourcedownTests {
+  
+  var testRootFolder = "src/test/resources/test"
+  var inputFolder = "input"
+  var expectedFolder = "expected"
+  var outputRootFolder = "build/tests"
+
+  var sourcedownBuild = "build/libs/sourcedown.jar"
+
+  def main(args: Array[String]) {
+
+    testRootFolder.children().foreach(executeTest)
+  }
+
+  /*
+    All tests are executed as an external process. Because that's generally
+    how people will use sourcedown. Or that's how I *anticipate* it being
+    used.
+  */
+  def executeTest(testFolder: Path) {
+    Console.println("Running test " + testFolder.name)
+
+    var status = ""
+    var ex: Exception = null
+
+    try {
+      var outputFolder = prepareOutputFolder(testFolder)
+
+      var execTest = Process("java -jar " + sourcedownBuild + 
+                             " -o " + outputFolder +
+                             " -r " + testFolder)
+  
+      var result = execTest.run(true)
+
+      if (result.exitValue != 0) {
+        status = Console.RED + "FAIL " + testFolder.name + Console.RESET
+      } else {
+        status = validateTest(testFolder, outputFolder)
+      }
+    } catch {
+      case e : Exception => {
+        status = Console.RED + e.getMessage + Console.RESET
+        ex = e
+      }
+    }
+  
+    Console.println(status)
+
+    if (ex != null) {
+      ex.printStackTrace()
+    }
+  }
+
+  /*
+    Validation is done by running the diff tool. If we get any output then
+    the test failed (and dump the damn output)
+  */
+  def validateTest(testFolder :Path, outputFolder :Path): String = {
+    val expected = testFolder / "expected"
     
-    var testRootFolder = "test/resources/test"
-    var inputFolder = "input"
-    var expectedFolder = "expected"
-    var outputRootFolder = "build/tests"
+    var execDiff = Process("diff " + expected.path + " " + outputFolder.path)
 
-    def main(args: Array[String]) {
-        testRootFolder.listFiles.foreach(executeTest)
+    var diffProcess = execDiff.run(true)
+
+    if (diffProcess.exitValue == 0) {
+      return Console.GREEN + "OK " + testFolder.name + Console.RESET        
+    } else {
+      return Console.RED + "FAIL " + testFolder.name + Console.RESET
+    }
+  }
+
+  /*
+    Makes sure the output folder exists for a particular input folder and
+    returns the output test folder.
+  */
+  def prepareOutputFolder(testFolder: Path): Path = {
+    var outputFolder = path(outputRootFolder + "/" + testFolder.name)
+
+    if (outputFolder.exists) {
+      outputFolder.deleteRecursively(true)
     }
 
-    /*
-        All tests are executed as an external process. Because that's generally
-        how people will use sourcedown. Or that's how I *anticipate* it being
-        used.
-    */
-    def executeTest(testFolder: File) {
-        
-    }
+    outputFolder.createDirectory()
 
-    implicit def file(path: String): File =
-        new File(path)
+    return outputFolder
+  }
+
+  implicit def path(path: String): Path = Path(path)
 }
